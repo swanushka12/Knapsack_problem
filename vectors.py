4d9af7bbbb6bb963e6623a902f6178022e611abc
swanushka12 <115592590+swanushka12@users.noreply.github.com>
2026-02-27 12:21:58 +0300

    Update vectors.py

--- vectors.py
+++ vectors.py
@@ -1,13 +1,13 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv("вфеф.csv")
+df = pd.read_csv("data.csv")
 
-plt.figure(figsize=(10,7))
+plt.figure(figsize = (10,7))
 
 for bits in sorted(df["element_bits"].unique()):
     sub = df[df["element_bits"] == bits].sort_values("k")
-    plt.plot(sub["k"], sub["if_ns"], marker='o', linewidth=2, label=f"{bits} бит")
+    plt.plot(sub["k"], sub["if_ns"], marker = 'o', linewidth = 2, label = f"{bits} бит")
 
 plt.xlabel("Число единичных координат k")
 plt.ylabel("Время (нс)")
